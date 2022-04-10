8aa170c4998068070844dbb957ed0aa972058c3c
Mikael Syska <mikael@syska.dk>
2022-04-11 01:49:28 +0200

    Support for build on linux.

--- PElshen.NoUiSlider.Blazor/gulpfile.js
+++ PElshen.NoUiSlider.Blazor/gulpfile.js
@@ -2,9 +2,12 @@
 var concat = require('gulp-concat');
 var minify = require('gulp-minify');
 var cleanCss = require('gulp-clean-css');
+var path = require('path');
+
+var outPath = path.join('wwwroot', 'dist')
 
 gulp.task('pack-js', function () {
-    return gulp.src(['wwwroot/js/lib/wNumb.js', 'wwwroot/js/lib/nouislider.js', 'wwwroot/js/*.js'])
+    return gulp.src([path.join('wwwroot', 'js', 'lib', 'wNumb.js'), path.join('wwwroot', 'js', 'lib', 'nouislider.js'), path.join('wwwroot', 'js', '*.js')])
         .pipe(concat('bundle.js'))
         .pipe(minify({
             ext: {
@@ -12,26 +15,26 @@ gulp.task('pack-js', function () {
             },
             noSource: true
         }))
-        .pipe(gulp.dest('wwwroot/dist'));
+        .pipe(gulp.dest(outPath));
 });
 
 gulp.task('pack-js-dev', function () {
-    return gulp.src(['wwwroot/js/lib/wNumb.js', 'wwwroot/js/lib/nouislider.js', 'wwwroot/js/*.js'])
+    return gulp.src([path.join('wwwroot', 'js', 'lib', 'wNumb.js'), path.join('wwwroot', 'js', 'lib', 'nouislider.js'), path.join('wwwroot', 'js', '*.js')])
         .pipe(concat('bundle.js'))
-        .pipe(gulp.dest('wwwroot/dist'));
+        .pipe(gulp.dest(outPath));
 });
 
 gulp.task('pack-css', function () {
-    return gulp.src(['wwwroot/css/lib/*.css', 'wwwroot/css/*.css'])
+    return gulp.src([path.join('wwwroot', 'css', 'lib', '*.css'), path.join('wwwroot', 'css', '*.css')])
         .pipe(concat('styles.css'))
         .pipe(cleanCss())
-        .pipe(gulp.dest('wwwroot/dist'));
+        .pipe(gulp.dest(outPath));
 });
 
 gulp.task('pack-css-dev', function () {
-    return gulp.src(['wwwroot/css/lib/*.css', 'wwwroot/css/*.css'])
+    return gulp.src([path.join('wwwroot', 'css', 'lib', '*.css'), path.join('wwwroot', 'css', '*.css')])
         .pipe(concat('styles.css'))
-        .pipe(gulp.dest('wwwroot/dist'));
+        .pipe(gulp.dest(outPath));
 });
 
 gulp.task('default', gulp.series(['pack-js', 'pack-css']));
